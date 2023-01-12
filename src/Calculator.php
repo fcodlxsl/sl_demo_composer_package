c71d39ea49ae366223cb898519f623157f252e94
Francisco <fcruz@sproutloud.com>
2023-01-12 08:43:12 -0600

    creation class Calculator and Test

---
+++ src/Calculator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SlDemoComposerPackage\NarcissusLibrary;
+
+class Calculator
+{
+
+    public function sum($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function minus($a, $b)
+    {
+        return $a - $b;
+    }
+
+}
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SlDemoComposerPackage\NarcissusLibrary\Tests;
+
+use SlDemoComposerPackage\NarcissusLibrary\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+     public function itSums()
+     {
+        $calculator = new Calculator();
+        $sum = $calculator->sum(5, 10);
+
+        $this->assertSame(15, $sum);
+     }
+
+     /**
+      * @test
+      */
+      public function itMinus()
+      {
+         $calculator = new Calculator();
+        $sum = $calculator->minus(10, 5);
+
+        $this->assertSame(5, $sum);
+      }
+}
